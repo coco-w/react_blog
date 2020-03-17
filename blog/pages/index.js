3b78b374de11c9fe5aad6ee73bed4fce3ec6e215
coco-desktop <535346881@qq.com>
2020-03-17 23:10:32 +0800

    test

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -9,38 +9,38 @@ import '../static/style/components/index.css'
 import { Row, Col, List, Icon } from 'antd'
 import Main from '../components/Main'
 const Home = (list) => {
-    const [myList, setMyList] = useState(
-     list.data
-    )    
-    return (
-      <Main title="Home">
-        <div key="left">
-               <List 
-               header={<div>最新日志</div>}
-               itemLayout="vertical"
-               dataSource={myList}
-               renderItem={item => (
-                 <List.Item>
-                  <div className="list-title">
-                    <Link href={{pathname: '/detailed',query: {id: item.id}}}>
-                      <a>
-                        {item.title}
-                      </a>                      
-                    </Link>                    
-                  </div>
-                  <div className="list-icon">
-                    <span><Icon type="calendar"/> {item.addTime} </span>
-                    <span><Icon type="folder"/> {item.typeName} </span>
-                    <span><Icon type="fire"/> {item.viewCount}人 </span>
-                  </div>
-                  <div className="list-context">{item.introduce}</div>
-                 </List.Item>
-               )}
-             />
-        </div>
-        <Author key="right"></Author>  
-      </Main>
-    )
+  const [myList, setMyList] = useState(
+    list.data
+  )    
+  return (
+    <Main title="Home">
+      <div key="left">
+              <List 
+              header={<div>最新日志</div>}
+              itemLayout="vertical"
+              dataSource={myList}
+              renderItem={item => (
+                <List.Item>
+                <div className="list-title">
+                  <Link href={{pathname: '/detailed',query: {id: item.id}}}>
+                    <a>
+                      {item.title}
+                    </a>                      
+                  </Link>                    
+                </div>
+                <div className="list-icon">
+                  <span><Icon type="calendar"/> {item.addTime} </span>
+                  <span><Icon type="folder"/> {item.typeName} </span>
+                  <span><Icon type="fire"/> {item.viewCount}人 </span>
+                </div>
+                <div className="list-context">{item.introduce}</div>
+                </List.Item>
+              )}
+            />
+      </div>
+      <Author key="right"></Author>  
+    </Main>
+  )
 }
 
 Home.getInitialProps = async ()=>{
