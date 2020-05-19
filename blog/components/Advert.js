9ae38b746493483cc2c94afd89034a72ac54904e
coco-desktop <535346881@qq.com>
2020-05-19 23:29:04 +0800

    添加广告信息

--- blog/components/Advert.js
+++ blog/components/Advert.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getAD } from '../api/default'
 import Router from 'next/router'
+import {Divider} from 'antd'
 import '../static/style/components/Advert.less'
 const Advert = () => {
   const [Ads, setAds] = useState([])
@@ -11,6 +12,7 @@ const Advert = () => {
   }, [])
   return (
     <div className="ad-div comm-box">
+      <Divider>广告信息</Divider> 
       {Ads.map(ele => {
         return (
           <div key={ele.id}>
