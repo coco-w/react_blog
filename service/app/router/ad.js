879b148f2ba1ae86206bee12439a2c7110167dca
coco-desktop <535346881@qq.com>
2020-05-20 23:17:06 +0800

    删除getAd

--- service/app/router/ad.js
+++ service/app/router/ad.js
@@ -2,6 +2,6 @@
 module.exports = app => {
   const { router, controller } = app
   const adminauth = app.middleware.adminauth()
-  router.get('/ad/getAD', adminauth, controller.default.ad.getAD)
+  router.get('/ad/getAD', controller.default.ad.getAD)
   router.post('/ad/updateAD', adminauth, controller.default.ad.updateAD)
 };
